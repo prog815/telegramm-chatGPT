ca93d48df1f3d16ba47b08ea75b30705370b4006
prog <evprog@gmail.com>
2023-03-05 17:44:56 +0500

    работает без сохранения контента

--- main.py
+++ main.py
@@ -1,3 +1,28 @@
 import openai
 from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+import sys
 
+token = sys.argv[1]
+
+openai.api_key = sys.argv[2]
+
+bot = Bot(token)
+dp = Dispatcher(bot)
+
+@dp.message_handler()
+async def send(message : types.Message):
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=message.text,
+        temperature=0.9,
+        max_tokens=1000,
+        top_p=1.0,
+        frequency_penalty=0.0,
+        presence_penalty=0.6,
+        stop=[" Human:", " AI:"])
+    
+    await message.answer(response['choices'][0]['text'])
+    
+executor.start_polling(dp, skip_updates=True)
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Серверная часть Telegram-бота для ChatGPT.
+
+
+По мотивам [ChatGPT OpenAI в Telegram на Python / Пишем Telegram-бота ChatGPT на Питоне](https://www.youtube.com/watch?app=desktop&v=7X1zxEzQGGM).
+
